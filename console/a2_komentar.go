e5cab4339a2106e449c00c16e100240e65a9463b
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2019-08-14 11:45:43 +0700

    menambhakan komentar pada file project

---
+++ console/a2_komentar.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    // menampilkan pesan hello world dengan komentar single line
+    fmt.Println("hello world")
+
+    // fmt.Println("baris ini tidak akan di eksekusi")
+
+    /*
+    	bagian ini menggunakna komentar multiline
+	fmt.Println("Ini jg tidak akan dieksekusi")
+    */
+}
